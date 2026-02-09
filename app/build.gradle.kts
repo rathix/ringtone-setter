dd5f31607e6a4f2a87bf143e2e4eff49e0a12dc5
Kenny Andries <mail@kennyandries.com>
2026-02-09 16:21:36 +0100

    Add unit tests and run them in workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,13 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:5.11.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
