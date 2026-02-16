bb40e6b8d84f60d350f0cbee864fc4c5a6c41a64
Kenny Andries <62221404+rathix@users.noreply.github.com>
2026-02-16 13:28:02 +0100

    Bump versionCode to 2 and versionName to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.kennyandries.ringtonesetter"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     signingConfigs {
